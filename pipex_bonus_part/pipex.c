8750282081bcbb5dcfe0ee7a8d03179b4b8b66ed
lechalme <vs.tretyakov01@gmail.com>
2022-01-19 19:41:30 +0300

    fixed and overwrite pipex_bonus

--- pipex_bonus_part/pipex.c
+++ pipex_bonus_part/pipex.c
@@ -6,123 +6,76 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void	child_proc(int *pipe_fd, char **av, char **envp)
+void	do_exec(char **av, char **envp, int i)
 {
-	char **cmd;
-	int in_file;
+	char	**cmd;
 
+	cmd = ft_split(av[i], ' ');
 
-	cmd = ft_split(av[2], ' ');
-	dup2(pipe_fd[1], 1);
-	close(pipe_fd[0]);
-	close(pipe_fd[1]);
-	in_file = open(av[1], O_RDONLY);
-	dup2(in_file, 0);
 	if (execve(get_path(envp, cmd[0]), cmd, envp) == -1)
 	{
 		ft_putstr_fd("pipex: command not found: ", 2);
 		ft_putstr_fd("\n", 2);
+		free(cmd);
 		exit(0);
 	}
+	free(cmd);
 }
 
-void 	master_proc(int **pipe_fd, int i, char **av, char **envp)
+int	ft_redirect(char **av, char **env, int in_file, int i)
 {
-	char **cmd;
-	int out_file;
+	int		pid;
+	pid_t	pipe_fd[2];
 
-	out_file = open(av[4], O_RDWR | O_CREAT | O_TRUNC, 777);
-	wait (0);
-	close(pipe_fd[i][1]);
-	cmd = ft_split(av[3], ' ');
-	dup2(pipe_fd[i][0], 0);
-	close(pipe_fd[i][0]);
-	dup2(out_file, 1);
-	if (execve(get_path(envp, cmd[0]), cmd, envp) == -1)
+	if (pipe(pipe_fd) == -1)
+		return (1);
+	pid = fork();
+	if (pid == -1)
 	{
-		ft_putstr_fd("pipex: command not found: ", 2);
-		ft_putstr_fd("\n", 2);
-		exit(0);
+		ft_putstr_fd("Fork failed\n", 2);
+		return (1);
 	}
+	if (pid)
+	{
+		close(pipe_fd[1]);
+		dup2(pipe_fd[0], 0);
+		waitpid(pid, NULL, 0);
+	}
+	else
+	{
+		close(pipe_fd[0]);
+		dup2(pipe_fd[1], 1);
+		if (in_file == 0)
+			exit(1);
+		else
+			do_exec(av, env, i);
+	}
+	return (0);
 }
 
-int		main(int ac, char **av, char **envp)
+int	main(int ac, char **av, char **envp)
 {
-	int		i;
-	int		**pipe_fd;
-	int		command;
-	int		num_cmd;
-	pid_t	pid;
-	int		j;
-	char **cmd;
+	int 	in_file;
+	int 	out_file;
+	int 	i;
 
-	num_cmd = ac - 3;
-	i = 0;
-	command = 0;
-	pipe_fd = malloc(sizeof(int*) * (ac - 3));
-	/* init pipes */
-	while (i < (ac -3))
+	i = 3;
+
+	if (ac >= 5)
 	{
-		pipe_fd[i] = malloc(sizeof(int*) * 2);
-		if (pipe(pipe_fd[i]) == -1)
-		{
-			perror("pipe error");
-			exit(0);
-		}
-		i++;
+		in_file = open(av[1], O_RDONLY);
+		out_file = open(av[ac - 1], O_RDWR | O_CREAT | O_TRUNC, 777);
+		dup2(in_file, 0);
+		dup2(out_file, 1);
+		ft_redirect(av, envp, in_file, 2);
+		while (i < ac - 2)
+			ft_redirect(av, envp, in_file, i++);
+		do_exec(av, envp, i);
 	}
-	i = 0;
-	while (command < num_cmd)
+	else
 	{
-		pid = fork();
-		if (i == 0)
-		{
-			child_proc(pipe_fd[i], av, envp);
-			i++;
-		}
-		if (pid == 0)
-		{
-			if (i > 0)
-			{
-				if (dup2(pipe_fd[i][0], 0) == -1)
-				{
-					perror("dup2 input error");
-					exit(0);
-				}
-				if (i != num_cmd -1)
-				{
-					if (dup2(pipe_fd[i][1], 1) == -1)
-					{
-						perror("dup2 output error");
-						exit(0);
-					}
-				}
-				close(pipe_fd[i][1]);
-				close(pipe_fd[i][0]);
-				cmd = ft_split(av[i + 2], ' ');
-				if (execve(get_path(envp, cmd[0]), cmd, envp) == -1)
-				{
-					perror("pipex: command not found: ");
-					exit(0);
-				}
-				free(cmd);
-				i++;
-			}
-		}
-		else if (pid < 0)
-		{
-			perror("incorrect pid");
-			exit (0);
-		}
-		command++;
-	}
-	master_proc(pipe_fd, ac, av, envp);
-	i = 0;
-	while (i < 2 * (num_cmd - 1))
-	{
-		close(pipe_fd[i][0]);
-		close(pipe_fd[i][1]);
-		i++;
+		ft_putstr_fd("Usage : ./pipex infile cmd1 cmd2 outfile\n", 2);
+		return (1);
 	}
 	return (0);
 }
